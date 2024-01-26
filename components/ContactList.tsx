9e45c7bde3be605a1e642bb71a7546f5688dd6d4
Rushikesh Chougule <46069967+rushikesh3299@users.noreply.github.com>
2024-01-26 11:44:57 +0900

    initial commit

---
+++ components/ContactList.tsx
@@ -0,0 +1,82 @@
+import {ScrollView, StyleSheet, Text, View, Image} from 'react-native';
+import React from 'react';
+
+export default function ContactList() {
+  const contacts = [
+    {
+      uid: 1,
+      name: 'Hitesh Choudhary',
+      status: 'Just an extra ordinary teacher',
+      imageUrl: 'https://avatars.githubusercontent.com/u/11613311?v=4',
+    },
+    {
+      uid: 2,
+      name: 'Anurag Tiwari',
+      status: 'I ❤️ To Code and Teach!',
+      imageUrl: 'https://avatars.githubusercontent.com/u/94738352?v=4',
+    },
+    {
+      uid: 3,
+      name: 'Sanket Singh',
+      status: 'Making your GPay smooth',
+      imageUrl: 'https://avatars.githubusercontent.com/u/29747452?v=4',
+    },
+    {
+      uid: 4,
+      name: 'Anirudh Jwala',
+      status: 'Building secure Digital banks',
+      imageUrl: 'https://avatars.githubusercontent.com/u/25549847?v=4',
+    },
+  ];
+  return (
+    <View>
+      <Text style={styles.headingText}>ContactList</Text>
+      <ScrollView style={styles.container} scrollEnabled={false}>
+        {contacts.map(({uid, name, status, imageUrl}) => (
+          <View key={uid} style={styles.userCard}>
+            <Image source={{uri: imageUrl}} style={styles.userImage} />
+            <View>
+              <Text style={styles.userName}>{name}</Text>
+              <Text style={styles.userStatus}>{status}</Text>
+            </View>
+          </View>
+        ))}
+      </ScrollView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  headingText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    paddingHorizontal: 8,
+  },
+  container: {
+    paddingHorizontal: 16,
+    marginBottom: 10,
+  },
+  userCard: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 4,
+    backgroundColor: '#8D3DAF',
+    padding: 4,
+    borderRadius: 8,
+  },
+  userImage: {
+    height: 60,
+    width: 60,
+    borderRadius: 30,
+    marginRight: 14,
+  },
+  userName: {
+    fontSize: 16,
+    fontWeight: '600',
+    color: '#FFF',
+  },
+  userStatus: {
+    fontSize: 12,
+    color: '#EEE',
+  },
+});
